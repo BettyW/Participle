b0d2dcd53388f6f1d78c67b704b50c9556f38c24
BettyW <276439029@qq.com>
2020-04-23 12:54:31 +0800

    Add files via upload
    20200423结巴分词相关代码上传2

---
+++ Participle/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Participle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txt_Result = new System.Windows.Forms.TextBox();
+            this.txt_Original = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.ShowToolTips = true;
+            this.tabControl1.Size = new System.Drawing.Size(776, 415);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "结巴分词";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.radioButton5);
+            this.tabPage1.Controls.Add(this.radioButton4);
+            this.tabPage1.Controls.Add(this.radioButton3);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.txt_Result);
+            this.tabPage1.Controls.Add(this.txt_Original);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 389);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "结巴分词";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(443, 153);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(71, 16);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "歧义消除";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(329, 153);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(95, 16);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "搜索引擎模式";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(236, 153);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(71, 16);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "新词识别";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(147, 153);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "精确模式";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(67, 153);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "全模式";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // txt_Result
+            // 
+            this.txt_Result.Location = new System.Drawing.Point(67, 199);
+            this.txt_Result.Multiline = true;
+            this.txt_Result.Name = "txt_Result";
+            this.txt_Result.ReadOnly = true;
+            this.txt_Result.Size = new System.Drawing.Size(597, 161);
+            this.txt_Result.TabIndex = 2;
+            // 
+            // txt_Original
+            // 
+            this.txt_Original.Location = new System.Drawing.Point(67, 61);
+            this.txt_Original.Multiline = true;
+            this.txt_Original.Name = "txt_Original";
+            this.txt_Original.Size = new System.Drawing.Size(597, 68);
+            this.txt_Original.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "分词";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 389);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "盘古分词";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txt_Result;
+        private System.Windows.Forms.TextBox txt_Original;
+    }
+}
+
---
+++ Participle/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using JiebaNet.Segmenter;
+
+namespace Participle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 结巴分词部分
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var original = txt_Original.Text;
+            var result = "";
+
+
+            var segment = new JiebaSegmenter();
+
+
+            //全模式
+            if (radioButton1.Checked == true)
+            {
+                var segments = segment.Cut(original, cutAll: true);
+                result = "【全模式】：" + string.Join("/", segments);
+
+                //e.g.我来到北京清华大学
+                Console.WriteLine("【全模式】：{0}", string.Join("/ ", segments));
+            }
+
+            //精确模式
+            // 默认为精确模式
+            if (radioButton2.Checked == true)
+            {
+                var segments = segment.Cut(original);
+                result = "【精确模式】：" + string.Join("/", segments);
+
+                //e.g.我来到北京清华大学
+                Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments));
+            }
+
+            //新词识别
+            // 默认为精确模式，同时也使用HMM模型
+            if (radioButton3.Checked == true)
+            {
+                var segments = segment.Cut(original);
+                result = "【新词识别】：" + string.Join("/", segments);
+
+                //e.g.他来到了网易杭研大厦  
+                Console.WriteLine("【新词识别】：{0}", string.Join("/ ", segments));
+            }
+
+            //搜索引擎模式
+            if (radioButton4.Checked == true)
+            {
+                //匹配这些中文标点符号 。 ？ ！ ， 、 ； ： “ ” ‘ ' （ ） 《 》 〈 〉 【 】 『 』 「 」 ﹃ ﹄ 〔 〕 … — ～ ﹏ ￥
+                var reg = "/[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5] /";
+
+
+
+                var segments = segment.CutForSearch(original);
+                result = "【搜索引擎模式】：" + string.Join("/", segments);
+
+
+
+                //e.g.小明硕士毕业于中国科学院计算所，后在日本京都大学深造
+                Console.WriteLine("【搜索引擎模式】:{0}", string.Join("/", segments));
+
+
+            }
+
+            //歧义消除
+            if (radioButton5.Checked == true)
+            {
+                var segments = segment.Cut(original);
+                result = "【歧义消除】：" + string.Join("/", segments);
+
+
+                //结过婚的和尚未结过婚的
+                Console.WriteLine("【歧义消除】：{0}", string.Join("/ ", segments));
+            }
+
+
+
+
+            txt_Result.Text = result;
+
+        }
+    }
+}
